4df5af5a04d74f83843ef6e55164987645137a45
Mounir <mounirmaadour@msn.com>
2023-11-17 16:42:45 +0100

    Ajout de l'exercice02 Javascript

--- Formation_CDA/07_JavaScript/Exercices/Exercice02/main.js
+++ Formation_CDA/07_JavaScript/Exercices/Exercice02/main.js
@@ -4,9 +4,7 @@ let nb1 = parseInt(prompt("Entrez le premier nombre :"));
 let nb2 = parseInt(prompt("Entrez le deuxième nombre :"));
 
 
-  // Calculer la somme des deux nombres
   let resultat = nb1 + nb2;
 
-  // Afficher le résultat
   console.log(`${nb1} + ${nb2} = ${resultat}`);
   alert(`${nb1} + ${nb2} = ${resultat}`);
