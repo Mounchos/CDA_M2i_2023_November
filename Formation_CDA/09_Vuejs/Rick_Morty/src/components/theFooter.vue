03c4d259b666f4b424ccdb26fb14e0a1827ae267
Mounchos <mounirmaadour@msn.com>
2023-12-18 16:45:08 +0100

    Ajout du TP Rick_Morty

--- Formation_CDA/09_Vuejs/Rick_Morty/src/components/theFooter.vue
+++ Formation_CDA/09_Vuejs/Rick_Morty/src/components/theFooter.vue
@@ -4,15 +4,10 @@ import btnFooter from './btnFooter.vue';
 
 <template>
     <footer
-        class="flex flex-col items-center justify-between w-full px-0 py-2 space-y-2 overflow-hidden text-4xl h-28 superSmall:h-full bg-darkGreen superSmall:px-10 superSmall:flex-row superSmall:space-y-0 superSmall:py-0">
+        class="flex flex-col justify-center items-center w-full px-0 py-2 space-y-2 overflow-hidden text-4xl h-28 superSmall:h-full bg-darkGreen superSmall:px-10 superSmall:flex-row superSmall:space-y-0 superSmall:py-0">
         <btnFooter
             class="animate__fadeInLeft"
-            btnText="GitHub Do Projeto"
-            btnLink="https://github.com/ericrq/RickAndMorty" />
-
-        <btnFooter
-            class="animate__fadeInRight"
-            btnText="GitHub Da API"
-            btnLink="https://github.com/afuh/rick-and-morty-api" />
+            btnText="Mounir MAADOUR"
+            btnLink="https://github.com/Mounchos/CDA_M2i_2023_November/tree/main/Formation_CDA/09_Vuejs/Rick_Morty"/>
     </footer>
 </template>
